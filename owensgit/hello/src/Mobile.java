729305c07951cd0ac8a20dd37b4345fd9df92ca7
owen <649073762@qq.com>
2019-08-11 17:59:31 +0800

    改变palyer中的computeLevel方法，添加判断level是straight，flush, straight_flush的方法体，使测试通过

--- owensgit/hello/src/Mobile.java
+++ owensgit/hello/src/Mobile.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+public  abstract  class Mobile {
+    private  String name;
+    private String brand;
+    private String color;
 
-public class Mobile {
+    public Mobile(String name, String brand, String color) {
+        this.name = name;
+        this.brand = brand;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public abstract void call(String message);
 }
